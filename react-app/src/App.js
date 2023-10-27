5639e533873033eb4f3a4f5b7441d3bd26deb16f
HE210401 <geamie@naver.com>
2023-10-27 16:01:21 +0900

    #6.1 useEffect

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,12 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("call an api");   // 여러번 반복됨
+  console.log("i run all the time");   // 여러번 반복됨
+  useEffect(() => {
+    console.log("CALL THE API....");
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
@@ -12,4 +15,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; 
